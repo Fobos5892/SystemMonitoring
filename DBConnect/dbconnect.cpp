0e5adbf1d8d9f1d65919fdf8cc6ffc66fa4a2ef7
Serafim5892 <fobos5892@gmail.com>
2026-06-30 12:05:31 +0300

    init repo and mainWindow
    Signed-off-by: Serafim5892 <fobos5892@gmail.com>

---
+++ DBConnect/dbconnect.cpp
@@ -0,0 +1,3 @@
+#include "dbconnect.h"
+
+DBConnect::DBConnect() {}
---
+++ DBConnect/dbconnect.h
@@ -0,0 +1,10 @@
+#ifndef DBCONNECT_H
+#define DBCONNECT_H
+
+class DBConnect
+{
+public:
+    DBConnect();
+};
+
+#endif // DBCONNECT_H
